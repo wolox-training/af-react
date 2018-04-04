90588fd5d4c465e73b3f63884b763b65534f097e
Axel Fratoni <afratoni@itba.edu.ar>
2018-04-04 14:24:17 -0300

    Code style fixes

--- React/wbooks/src/Services/UserService.js
+++ React/wbooks/src/Services/UserService.js
@@ -1,7 +1,7 @@
 import api from '../Config/api';
 
 const UserService = {
-  login: async user => api.post('/users/sessions', { ...user }),
+  login: async user => api.post('/users/sessions', user),
   signup: async user => api.post('/users', { user }),
   books: async () => api.get('/books'),
   bookDetail: async id => api.get(`/books/${id}`)
--- React/wbooks/src/redux/auth/actions.js
+++ React/wbooks/src/redux/auth/actions.js
@@ -4,14 +4,21 @@ import api from '../../Config/api';
 import history from '../../Config/history';
 import localStorage from '../../Services/LocalStorage';
 import UserService from '../../Services/UserService';
+import { routes } from '../../Config/routes';
 
-export const actions = createTypes(completeTypes(['LOGOUT', 'LOGIN', 'SIGNUP']), '@@AUTH');
+export const actions = createTypes(completeTypes(['LOGOUT', 'LOGIN'], ['SIGNUP']), '@@AUTH');
+
+function saveTokenAndRedirect(token) {
+  localStorage.setSessionToken(token);
+  api.setHeader('Authorization', token);
+  history.push(routes.PRIVATE_ROUTE);
+}
 
 const ActionCreators = {
   logout: () => dispatch => {
     localStorage.removeSessionToken();
     api.setHeader('Authorization', '');
-    history.push('/login');
+    history.push(routes.ANONYMOUS_ROUTE);
     dispatch({ type: actions.LOGOUT });
   },
   login: (email, password) => ({
@@ -19,26 +26,18 @@ const ActionCreators = {
     target: 'login',
     service: UserService.login,
     payload: { email, password },
-    injections: [
-      withPostSuccess((dispatch, response) => {
-        localStorage.setSessionToken(response.data.access_token);
-        api.setHeader('Authorization', response.data.access_token);
-        history.push('/');
-      })
-    ]
+    injections: withPostSuccess((dispatch, response) => {
+      saveTokenAndRedirect(response.data.access_token);
+    })
   }),
   signup: user => ({
     type: actions.SIGNUP,
     target: 'signup',
     service: UserService.signup,
     payload: user,
-    injections: [
-      withPostSuccess((dispatch, response) => {
-        localStorage.setSessionToken(response.data.access_token);
-        api.setHeader('Authorization', response.data.access_token);
-        history.push('/');
-      })
-    ]
+    injections: withPostSuccess((dispatch, response) => {
+      saveTokenAndRedirect(response.data.access_token);
+    })
   })
 };
 

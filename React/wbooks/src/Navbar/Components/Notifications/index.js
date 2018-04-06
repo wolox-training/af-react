091d243e715ef2f710bac2e4af15ff4b9e86264b
Axel Fratoni <afratoni@itba.edu.ar>
2018-04-06 15:37:12 -0300

    Dark style to notif

--- React/wbooks/src/Navbar/Components/Notifications/index.js
+++ React/wbooks/src/Navbar/Components/Notifications/index.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import BookCover from '../../../Components/BookCover';
+import withTheme from '../../../HOC/withTheme';
 
 import './style.css';
 
-function Notifications() {
+function Notifications({ theme }) {
   const exampleImage = 'http://wolox-training.s3.amazonaws.com/uploads/6942334-M.jpg';
   return (
-    <div className="notifications-container">
+    <div className={`notifications-container theme-${theme}`}>
       <div className="notification">
-        <BookCover imageUrl={exampleImage} imgStyle="book-img" defaultStyle="default-img" title="title" />
+        <BookCover
+          imageUrl={exampleImage}
+          imgStyle={`book-img theme-${theme}`}
+          defaultStyle="default-img"
+          title="title"
+        />
         <div className="data-container">
           <span className="book-status">Se encuentra disponible el libro</span>
-          <h3 className="book-title">Titulo</h3>
+          <h3 className={`book-title theme-${theme}`}>Titulo</h3>
           <span className="book-author">Autor</span>
         </div>
       </div>
       <div className="notification">
-        <BookCover imageUrl={exampleImage} imgStyle="book-img" defaultStyle="default-img" title="title" />
+        <BookCover
+          imageUrl={exampleImage}
+          imgStyle={`book-img theme-${theme}`}
+          defaultStyle="default-img"
+          title="title"
+        />
         <div className="data-container">
           <span className="book-status">Se encuentra disponible el libro</span>
-          <h3 className="book-title">Titulo</h3>
+          <h3 className={`book-title theme-${theme}`}>Titulo</h3>
           <span className="book-author">Autor</span>
         </div>
       </div>
@@ -28,4 +39,4 @@ function Notifications() {
   );
 }
 
-export default Notifications;
+export default withTheme(Notifications);
